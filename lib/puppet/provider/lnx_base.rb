d6f0cbe9e5ff30c098231222b76d0e13a7ddd810
Sergey Vasilenko <svasilenko@mirantis.com>
2014-12-07 16:41:18 +0800

    describe verdor_specific for default providers leads to fail
    + describe external_ids for non-ovs based providers leads to fail

--- lib/puppet/provider/lnx_base.rb
+++ lib/puppet/provider/lnx_base.rb
@@ -4,19 +4,17 @@
 class Puppet::Provider::Lnx_base < Puppet::Provider
 
   def external_ids
-    notice("Resource '#{@resource[:name]}': External_ids feature don't implemented for this provider.")
     return {}
   end
   def external_ids=(value)
-    notice("Resource '#{@resource[:name]}': External_ids feature don't implemented for this provider.")
+    fail("Resource '#{@resource[:name]}': External_ids feature don't implemented for this provider.")
   end
 
   def vendor_specific
-    notice("Resource '#{@resource[:name]}': Vendor_specific field don't implemented for default providers.")
     return {}
   end
   def vendor_specific=(value)
-    notice("Resource '#{@resource[:name]}': Vendor_specific field don't implemented for default providers.")
+    fail("Resource '#{@resource[:name]}': Vendor_specific field don't implemented for default providers.")
   end
 
 end
--- lib/puppet/provider/ovs_base.rb
+++ lib/puppet/provider/ovs_base.rb
@@ -4,11 +4,10 @@
 class Puppet::Provider::Ovs_base < Puppet::Provider
 
   def vendor_specific
-    notice("Resource '#{@resource[:name]}': Vendor_specific field don't implemented for default providers.")
     return {}
   end
   def vendor_specific=(value)
-    notice("Resource '#{@resource[:name]}': Vendor_specific field don't implemented for default providers.")
+    fail("Resource '#{@resource[:name]}': Vendor_specific field don't implemented for default providers.")
   end
 
 end
